d7a38e705c472a248088c08943dc7b45f0b72f5f
Tom Wu <cw223@duke.edu>
2016-03-12 17:47:10 -0500

    commit before removing snakes

--- Eye_OpenCV/eyes.py
+++ Eye_OpenCV/eyes.py
@@ -2,14 +2,6 @@ import cv2
 # import cv
 import numpy as np
 from copy import copy
-# import matlab
-
-# import sys
-# sys.path.append('C:/Users/User/OneDrive/Duke/6_Spring_2016/ECE_590/drowsiness-detection/Eye_OpenCV/morphsnakes')
-# import morphsnakes
-# import test_snakes
-# from matplotlib import pyplot as ppl
-# ppl.figure()
 
 # TODO: define path(s)
 # face_cascade = cv2.CascadeClassifier('C:/Users/User/Desktop/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml')
@@ -61,33 +53,6 @@ def find_eyes(img_in, eng):
     eye_region_colored = roi_color[ey:ey+eh, ex:ex+ew]
     eye_region = roi_gray[ey:ey+eh, ex:ex+ew]
     (height, width) = eye_region.shape[:2]
-
-    # # TODO
-    # gI = morphsnakes.gborders(eye_region, alpha=1000, sigma=5.48)
-    #
-    # # Morphological GAC. Initialization of the level-set.
-    # mgac = morphsnakes.MorphGAC(gI, smoothing=1, threshold=0.31, balloon=1)
-    # mgac.levelset = test_snakes.circle_levelset(eye_region.shape, (height/2, width/4), 20)
-    #
-    # # Visual evolution.
-    # morphsnakes.evolve_visual(mgac, num_iters=45, background=eye_region_colored)
-    # ppl.clf()
-
-    # (height, width) = roi_gray.shape[:2]
-    # bitmap = cv.CreateImageHeader((height, width), cv.IPL_DEPTH_8U, 1)
-    # cv.SetData(bitmap, roi_gray.tostring(),
-    #            roi_gray.dtype.itemsize * 3 * width)
-    # contour = np.array([(border, border), (border, height-border),
-    #                     (width-border, height-border), (width-border, border)],
-    #                    dtype=np.int)
-    # contour = [(border, border), (height-border, width-border)]
-    # contour = cv.SnakeImage(bitmap, contour, alpha, beta, gamma, (3, 3), (cv.CV_TERMCRIT_ITER | cv.CV_TERMCRIT_EPS, 10, 0.01))
-    # img_out = np.asarray(bitmap[:, :])
-
-    # roi_size = eye_region.shape[:2]
-    # m_img_in = matlab.uint8(eye_region)
-    # m_img_out = eng.get_eye(m_img_in)
-    # img_out = m_img_out.reshape(roi_size)
 
     img_out = roi_color
     return img_out
